3415bf84d4940fb792a9394f87b64b495e62806d
compainn <compainn@outlook.com>
2026-02-22 01:19:40 +0300

    Create config.py

---
+++ config.py
@@ -0,0 +1,17 @@
+TOKEN = '8537860677:AAGdyuWZbBysGsHLdPOIiU_yD1f-5DGA9V8'
+BOT_USERNAME = 'https://t.me/safneak_cube1_bot'
+CHANNEL_ID = '@comnataigr'
+CHANNEL_URL = 't.me/comnataigr'
+PROJECT_FAQ = '#'
+INFO_CHANNEL = 't.me/+Q7nC304lA1RkZTcy'
+SUPPORT = 't.me/ssupport_safarik_dice'
+MIN_AMOUNT = 0.15
+
+CRYPTO_BOT_TOKEN = "536072:AAz2uxUf7TnuebdNelMmBHJAnoJ0mP1Zcrs"
+CRYPTO_BOT_API_URL = "https://pay.crypt.bot/api"
+
+MIN_DEPOSIT = 0.3
+MIN_WITHDRAWAL = 0.30
+
+ADMIN_PASSWORD = "1337"
+ADMIN_IDS = []
