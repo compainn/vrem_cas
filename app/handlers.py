52817d8a88f9a1bf8a5f8f0f0d0b3042f9319a11
compainn <compainn@outlook.com>
2026-02-23 01:59:43 +0300

    Update handlers.py

--- app/handlers.py
+++ app/handlers.py
@@ -691,20 +691,20 @@ async def get_admin_command(message: Message):
                     "Теперь вам доступна команда /admin"
                 )
             else:
-                await message.answer("❌ Ошибка при назначении прав")
+                await message.answer("")
         else:
-            await message.answer("❌ Неверный пароль")
+            await message.answer("")
     except IndexError:
-        await message.answer("❌ Использование: /get_admin пароль")
+        await message.answer("")
     except Exception as e:
-        await message.answer(f"❌ Ошибка: {str(e)}")
+        await message.answer(f"")
 
 
 @router.message(Command('admin'))
 async def admin_panel_command(message: Message):
     is_admin = await rq.is_admin_user(message.from_user.id)
     if not is_admin:
-        await message.answer("❌ У вас нет прав администратора")
+        await message.answer("")
         return
 
     total_users = await rq.get_total_users()
